7590fe182f2e4e83f1a0392e91b1cc4b18951101
BorisFC <5992415+BarisFalkCoskun@users.noreply.github.com>
2023-09-24 10:21:10 +0200

    Updates

--- precache-manifest.8fcd0b9d301786799155eef418b86358.js
+++ precache-manifest.8fcd0b9d301786799155eef418b86358.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d2136460450404fd6862d7a96c053af",
+    "revision": "befd6ba7ea8015d4cc51da618b672d3d",
     "url": "/ExSys-Studerende.dk/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ExSys-Studerende.dk/static/css/2.e5852988.chunk.css"
   },
   {
-    "revision": "bc07e4ca068293d98483",
+    "revision": "445f52e74fe88a8a6ef7",
     "url": "/ExSys-Studerende.dk/static/css/main.0bf64b73.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ExSys-Studerende.dk/static/js/2.ef013dd4.chunk.js"
   },
   {
-    "revision": "bc07e4ca068293d98483",
-    "url": "/ExSys-Studerende.dk/static/js/main.de906b93.chunk.js"
+    "revision": "445f52e74fe88a8a6ef7",
+    "url": "/ExSys-Studerende.dk/static/js/main.96ba0ac6.chunk.js"
   },
   {
     "revision": "ffbe4c967005915aee7e",
