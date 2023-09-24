c3b71cc1a5cefdf3260aeda1b009ab905640e028
Baris Falk Coskun <5992415+BarisFalkCoskun@users.noreply.github.com>
2023-09-24 11:37:59 +0200

    Update mergeCal.hs

--- server/mergeCal.hs
+++ server/mergeCal.hs
@@ -13,7 +13,7 @@ import System.Environment
 import Text.Blaze.Html5 as H hiding (main, head, map)
 import qualified Text.Blaze.Html5 as H (head, map)
 import Text.Blaze.Html5.Attributes as A
-import Text.Blaze.Renderer.Pretty
+import Text.Blaze.Html.Renderer.Pretty
 import Data.Foldable
 import Data.Function
 import Data.String
@@ -61,8 +61,6 @@ main = do
       weekDates = getWeekDates (weekRange "20210516T075231Z" zt) cal3
       --weekDates = getWeekDates (weekRange (utctimeToISO8601 now) zt) cal3
     putStr (renderHtml (createSchema cal3 weekDates))
-  else
-    error "Program must have 1 or 2 arguments."
 
 createSchema :: ParsedCal -> WeekDates -> H.Html
 createSchema cal weekDates = docTypeHtml $ do
