e54f979da6af769e5bb094298e7fbd20074ad396
BorisFC <5992415+BarisFalkCoskun@users.noreply.github.com>
2023-09-24 09:43:42 +0200

    Updates

---
+++ precache-manifest.85f300dbef483f93c277b727a41bf157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1797671ccbf00d442282aa4dd7623088",
+    "url": "/ExSys-Studerende.dk/index.html"
+  },
+  {
+    "revision": "f5a9605e828284bac8b6",
+    "url": "/ExSys-Studerende.dk/static/css/2.e5852988.chunk.css"
+  },
+  {
+    "revision": "0d096a3aa29f36218d95",
+    "url": "/ExSys-Studerende.dk/static/css/main.0bf64b73.chunk.css"
+  },
+  {
+    "revision": "f5a9605e828284bac8b6",
+    "url": "/ExSys-Studerende.dk/static/js/2.ef013dd4.chunk.js"
+  },
+  {
+    "revision": "0d096a3aa29f36218d95",
+    "url": "/ExSys-Studerende.dk/static/js/main.d7746e6a.chunk.js"
+  },
+  {
+    "revision": "ffbe4c967005915aee7e",
+    "url": "/ExSys-Studerende.dk/static/js/runtime~main.d1fec68e.js"
+  }
+]);
